8d1376412db0f58167cb327fb161bf75ed1c2a18
yeonseoo <yeonseoo@gmail.com>
2024-03-20 13:48:38 +0900

    임시커밋

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 export default function About() {
     return (
         <section className="flex">
@@ -11,7 +13,10 @@ export default function About() {
                     우리의 활동은 웹, 앱, 모바일을 넘나들며 세상의 모든 비즈니스 활동에 강한 영향력을 전달합니다.
                 </span>
             </div>
-            <div className="flex-1">image</div>
+            <div className="flex-1 overflow-hidden relative">
+            <Image src="/images/content_01.svg" alt="content_01" width={1152} height={769} priority />
+            <span className='text-white text-40 absolute bottom-0 left-0 w-full whitespace-nowrap'>Creative Development Company BODA.Lab</span>
+            </div>
         </section>
     );
 }
